36dbc31f59d5d008fe72d032bd7b6d865d2a4a99
lijieming2 <lijieming2@yy.com>
2024-04-10 16:56:28 +0800

    macros

--- exercises/lifetimes/lifetimes2.rs
+++ exercises/lifetimes/lifetimes2.rs
@@ -20,8 +20,8 @@ fn main() {
     let string1 = String::from("long string is long");
     let result;
     {
-        let string2 = String::from("xyz");
-        result = longest(string1.as_str(), string2.as_str());
+        let string2 = "xyz";
+        result = longest(string1.as_str(), string2);
     }
     println!("The longest string is '{}'", result);
 }
